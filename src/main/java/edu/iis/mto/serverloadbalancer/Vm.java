facd993abc8f0ec31cf6fcbd87a22eadcafde889
Pawełek <p.dawiduk@gmail.com>
2016-09-05 00:06:46 +0200

    one server with 0 vms

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/Vm.java
@@ -0,0 +1,7 @@
+package edu.iis.mto.serverloadbalancer;
+
+/**
+ * Created by Pawełek on 04.09.2016.
+ */
+public class Vm {
+}
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -4,12 +4,44 @@ package edu.iis.mto.serverloadbalancer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
 	@Test
 	public void itCompiles() {
 		assertThat(true, equalTo(true));
+	}
+
+	@Test
+	public void balancingServernoVmServerStaysEmpty(){
+		Server server = a(server().withCapacity(1));
+		balancing(serverListWithServers(),emptyListOfVms());
+		assertThat(server, hastCurrentLoadOf(0.0d));
+	}
+
+	private Matcher<? super Server> hastCurrentLoadOf(double expectedLoadPercentage) {
+		return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
+	}
+
+	private void balancing(Server[] servers, Vm[] vms) {
+		new ServerLoadBalancer().balance(servers,vms);
+	}
+
+	private Vm[] emptyListOfVms() {
+		return new Vm[0];
+	}
+
+	private Server[] serverListWithServers(Server... servers) {
+		return servers;
+	}
+
+	private Server a(ServerBuilder serverBuilder) {
+		return serverBuilder.build();
+	}
+
+	private ServerBuilder server() {
+		return new ServerBuilder();
 	}
 
 
