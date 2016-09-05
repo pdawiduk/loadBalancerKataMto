b25925460d81aa45257f2ceb8240c01069ac4760
Pawełek <p.dawiduk@gmail.com>
2016-09-05 08:43:32 +0200

    refactoring

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,11 +1,16 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Pawełek on 05.09.2016.
  */
 public class Server {
+    public static final double MAXIMUM = 100.0d;
     public double currentLoadPercentage;
     public int capacity;
+    List<Vm> vmList = new ArrayList<Vm>();
 
     public boolean contains(Vm vm) {
         return true;
@@ -13,5 +18,9 @@ public class Server {
 
     public Server(int capacity) {
         this.capacity = capacity;
+    }
+
+    public void addVm(Vm vm) {
+        currentLoadPercentage =(double)vm.size/ (double) capacity * MAXIMUM;
     }
 }
